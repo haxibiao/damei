7bdbbd50bc055c9894b06535d3a392c587acd65f
xiaoyanfang <xiaoyanfang@haxibiao.com>
2020-03-31 11:35:17 +0800

    重构 TouchFeedback Badge

---
+++ src/components/TouchableView/TouchFeedback.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { TouchableOpacity, ViewStyle } from "react-native";
+import NetInfo from "@react-native-community/netinfo";
+
+function TouchFeedback(props: {
+    authenticated?: boolean;
+    style?: any;
+    activeOpacity?: number;
+    navigation?: any;
+    onPress?: any;
+    disabled?: any;
+    checkNetwork?: any;
+    key?: any;
+}) {
+    const authenticated = props?.authenticated ?? false;
+    const activeOpacity = props?.activeOpacity ?? 0.6;
+
+    const middleware = (callback: any, navigation: any) => {
+        return () => {
+            if (TOKEN) {
+                callback && callback();
+            } else {
+                navigation.navigate("Login");
+            }
+        };
+    };
+
+    const checkNetwork = (submit: any) => {
+        NetInfo.fetch().then(state => {
+            if (state.isConnected) {
+                submit && submit();
+            } else {
+                Toast.show({ content: "网络错误,请检查是否连接网络" });
+            }
+        });
+    };
+
+    const buildProps = () => {
+        let onPress = props?.onPress ?? null;
+        let navigation = props?.navigation ?? null;
+        let style = props.style;
+        let disabled = props.disabled ?? false;
+
+        if (authenticated && navigation) {
+            onPress = middleware(onPress, navigation);
+        }
+
+        if (props.checkNetwork) {
+            onPress = checkNetwork(onPress);
+        }
+
+        if (props.disabled) {
+            if (style instanceof Array) {
+                style = [
+                    {
+                        opacity: 0.6
+                    },
+                    ...style
+                ];
+            } else {
+                style = [
+                    {
+                        opacity: 0.6
+                    },
+                    style
+                ];
+            }
+        }
+        return { onPress, disabled, style };
+    };
+    let p = buildProps();
+    if (props?.key) return <TouchableOpacity {...p} key={props.key} />;
+    return <TouchableOpacity {...p} />;
+}
+
+export default TouchFeedback;
