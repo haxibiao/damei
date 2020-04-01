2e733b7122cc4a762f53cd842fda3d5d27ecd468
lzp <lzp@mbp.local>
2020-04-01 14:56:48 +0800

    修复反馈输入框聚焦时内容被挤出屏幕

--- src/screens/feedback/index.js
+++ src/screens/feedback/index.js
@@ -5,7 +5,7 @@
 'use strict';
 
 import React, { Component } from 'react';
-import { StyleSheet, View, ScrollView, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, ScrollView, Text,TextInput, TouchableOpacity } from 'react-native';
 
 import { Theme, SCREEN_WIDTH, PxFit } from 'utils';
 
@@ -17,6 +17,8 @@ import FeedbackList from './components/FeedbackList';
 
 import ScrollableTabView from 'react-native-scrollable-tab-view';
 
+import {Page} from '../../widgets';
+
 class index extends Component {
     constructor(props) {
         super(props);
@@ -26,28 +28,28 @@ class index extends Component {
     render() {
         const { navigation } = this.props;
         return (
-            <PageContainer hiddenNavBar contentViewStyle={{ marginTop: Theme.statusBarHeight }}>
-                <ScrollableTabView
+            <Page.PageCleared safe>
+            <ScrollableTabView
                     renderTabBar={props => (
                         <ScrollTabBar {...props} tabUnderlineWidth={PxFit(50)} underLineColor={Theme.primaryColor} />
                     )}>
                     <Feedback navigation={navigation} tabLabel='反馈建议' />
                     {!config.disableAd && <FeedbackList navigation={navigation} tabLabel='问题中心' />}
                 </ScrollableTabView>
-                <View style={styles.backButton}>
+                <View style={[styles.backButton,{top: Page.CONSTANTS.StatusBarHeight+2}]}>
                     <TouchFeedback activeOpacity={1} onPress={() => navigation.goBack()}>
                         <Iconfont name='left' color={Theme.defaultTextColor} size={PxFit(21)} />
                     </TouchFeedback>
                 </View>
-            </PageContainer>
-        );
+            </Page.PageCleared>
+        )
     }
 }
 
 const styles = StyleSheet.create({
     backButton: {
         position: 'absolute',
-        top: 0,
+
         left: 0,
         width: Theme.navBarContentHeight,
         height: Theme.navBarContentHeight,
