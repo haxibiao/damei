e6d60c6a787ff4f1e04be5e2e7409ad174a9d0b9
zengdawei <zengdawei@haxibiao.com>
2020-04-10 16:37:30 +0800

    请求广告管理接口时带上 version 等信息参数在 header

--- src/service/fetch/enableAdvert.js
+++ src/service/fetch/enableAdvert.js
@@ -1,28 +1,41 @@
-import { Config } from 'utils';
+import { Config } from "utils";
+import { Platform } from "react-native";
 
 //获取广告开启配置
 export default function(callback: Promise) {
-    Promise.race([
-        fetch(Config.ServerRoot + '/api/app-ad-manage?' + Date.now()),
-        new Promise((resolve, reject) => {
-            setTimeout(() => reject(new Error('request timeout')), 1500);
-        }),
-    ])
-        .then(response => response.json())
-        .then(result => {
-            callback(result);
-        })
-        .catch(err => {
-            callback({
-                enable_splash: false,
-                enable_question: true,
-                enable_reward: true,
-                enable_banner: true,
-                enable_feed: true,
-                disable: {
-                    huawei: false,
-                    ios: true,
-                },
-            });
-        });
+  const deviceHeaders = {};
+
+  deviceHeaders.os = Platform.OS; // 操作系统
+  deviceHeaders.build = Config.Build; // 手动修改的build版本号
+  deviceHeaders.referrer = Config.AppStore; // 应用商店来源
+  deviceHeaders.version = Config.Version; // 手动修改的App版本号
+  deviceHeaders.appid = Config.PackageName; // 手动修改的包名
+  deviceHeaders.package = Config.PackageName; // 手动修改的包名
+
+  Promise.race([
+    fetch(Config.ServerRoot + "/api/app-ad-manage?" + Date.now(), {
+      methos: "POST",
+      headers: { ...deviceHeaders }
+    }),
+    new Promise((resolve, reject) => {
+      setTimeout(() => reject(new Error("request timeout")), 1500);
+    })
+  ])
+    .then(response => response.json())
+    .then(result => {
+      callback(result);
+    })
+    .catch(err => {
+      callback({
+        enable_splash: false,
+        enable_question: true,
+        enable_reward: true,
+        enable_banner: true,
+        enable_feed: true,
+        disable: {
+          huawei: false,
+          ios: true
+        }
+      });
+    });
 }
