a79afa16ee807f4765efe6ad156ffe2f0827a142
xuan <gaoxuan@haxibiao.com>
2020-05-09 11:39:35 +0800

    DM-359  修复Tools.navigator的导航函数（navigation 5x）

--- src/routers/new_index.tsx
+++ src/routers/new_index.tsx
@@ -2,19 +2,18 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppStackNavigation from './AppStackNavigation';
 import { AppearanceProvider, useColorScheme } from 'react-native-appearance';
-
+import { Config, Tools } from 'utils';
 //白天,黑夜模式 主题(颜色)
-import {DayTheme,NightTheme} from './Theme';
+import { DayTheme, NightTheme } from './Theme';
 
 export default function Nav() {
-
     const schema = useColorScheme();
 
     return (
         <AppearanceProvider>
-            <NavigationContainer theme={schema === 'dark' ? NightTheme : DayTheme}>
+            <NavigationContainer theme={schema === 'dark' ? NightTheme : DayTheme} ref={Tools.setRootNavigation}>
                 <AppStackNavigation />
             </NavigationContainer>
         </AppearanceProvider>
-    )
+    );
 }
