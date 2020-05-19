baf2b13c91155714d4b581c5e9495bb2b2f826c2
高璇 <gaoxuan@haxibiao.com>
2020-05-19 09:22:45 +0800

    DM-372 修复微信登录异常

--- src/screens/login/index.js
+++ src/screens/login/index.js
@@ -254,33 +254,37 @@ class index extends Component {
         WeChat.isWXAppInstalled()
             .then((isInstalled) => {
                 if (isInstalled) {
-                    WeChat.sendAuthRequest(scope, state).then((responseCode) => {
-                        this.setState({
-                            submitting: false,
-                        });
-                        this.getWechatInfo(
-                            responseCode.code,
-                            (result) => {
-                                if (result.data && result.data.unionid) {
-                                    // 新用户绑定手机号
-                                    this.props.navigation.navigate('PhoneBind', {
-                                        data: result.data,
+                    WeChat.sendAuthRequest(scope, state)
+                        .then((responseCode) => {
+                            this.setState({
+                                submitting: false,
+                            });
+                            this.getWechatInfo(
+                                responseCode.code,
+                                (result) => {
+                                    if (result.data && result.data.unionid) {
+                                        // 新用户绑定手机号
+                                        this.props.navigation.navigate('PhoneBind', {
+                                            data: result.data,
+                                        });
+                                    } else {
+                                        // 老用户直接登录
+                                        this.getUserData(result.data.user);
+                                    }
+                                },
+                                () => {
+                                    Toast.show({
+                                        content: '微信身份信息获取失败，请检查微信是否登录',
                                     });
-                                } else {
-                                    // 老用户直接登录
-                                    this.getUserData(result.data.user);
+                                    this.setState({
+                                        submitting: false,
+                                    });
                                 }
-                            },
-                            () => {
-                                Toast.show({ content: '微信身份信息获取失败，请检查微信是否登录' });
-                                this.setState({
-                                    submitting: false,
-                                });
-                            }
-                        ).catch((err) => {
+                            );
+                        })
+                        .catch((err) => {
                             Toast.show({ content: '登录授权发生错误' });
                         });
-                    });
                 } else {
                     Toast.show({ content: '未安装微信或当前微信版本较低' });
                 }
