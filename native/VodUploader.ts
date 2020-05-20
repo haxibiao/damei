823c6712dc10870911c3af15e1dcc1312753d255
高璇 <gaoxuan@haxibiao.com>
2020-05-20 10:18:38 +0800

    DM-336 修复IOS上传视频

--- native/VodUploader.ts
+++ native/VodUploader.ts
@@ -2,15 +2,16 @@ import { NativeModules, DeviceEventEmitter, NativeEventEmitter, Platform } from 
 
 export type UploadEvent = 'resultVideo' | 'videoProgress';
 
-let module = NativeModules.VodVideoUploader;
+let module = Platform.OS === 'android' ? NativeModules.VodVideoUploader : NativeModules.VodUploader;
 let eventPrefix = 'VodUploader-';
 let deviceEmitter = Platform.OS === 'android' ? DeviceEventEmitter : module ? new NativeEventEmitter(module) : null;
 
-export const startUpload = (signature: string, videoPath: string): Promise<string> => module.beginUpload(signature, videoPath);
+export const startUpload = (signature: string, videoPath: string): Promise<string> =>
+    module.beginUpload(signature, videoPath);
 
 export const addListener = (eventType: UploadEvent, listener: Function) => {
-    return deviceEmitter.addListener(eventPrefix + eventType, (data : any) => {
-        // console.log(data);       
+    return deviceEmitter.addListener(eventPrefix + eventType, (data: any) => {
+        // console.log(data);
         if (data) {
             listener(data);
         }
